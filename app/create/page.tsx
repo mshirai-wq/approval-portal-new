a89d76702ab64e21b99eda93e5df07175e2dd92f
白井 元基 <m.shirai@yunia.local>
2026-06-26 13:50:05 +0900

    Fix build error by adding edge runtime declaration

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,4 @@
-export const dynamic = 'force-dynamic' // ★これで静的HTML化を強制解除します
+export const dynamic = 'force-dynamic'
+export const runtime = 'edge' // ★これを入れ忘れていました。本当にごめんなさい！
 import CreateForm from './CreateForm'
 export default function Page() { return <CreateForm /> }
